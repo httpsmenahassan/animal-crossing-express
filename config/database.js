09a0f7bc37a35e2dc6bc4787f49d3cb6f3da0e33
httpsmenahassan <whobutmenacodes@gmail.com>
2023-04-30 22:48:15 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+// config/database.js
+module.exports = {
+
+    // 'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/test?retryWrites=true&w=majority',
+
+    'url' : 'mongodb+srv://mh:test@animalcrossing.vfiofm4.mongodb.net/villagers?retryWrites=true&w=majority',
+    'dbName': 'villagers'
+};
